f13980471f06680e37ca99fdf7ae255c971f885d
nihalnaveed12 <nihalnaveed50@gmail.com>
2025-03-06 02:24:34 +0500

    Added Tailwind theme configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter , Poppins} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// Inter font
+const inter = Inter({ subsets: ["latin"], weight: ["400", "600", "700"], variable: "--font-inter" });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// Poppins font
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600", "700"], variable: "--font-poppins" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${poppins.variable}` }
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
 export default function Home() {
-  return <div>Clone</div>;
+  return (
+    <div className="">
+      <div className="bg-[var(--color-bg)] text-[var(--color-text)] p-6">
+        This is a dark background with white text
+      </div>
+
+      <div className="bg-gradient-to-r from-[var(--gradient-from)] to-[var(--gradient-to)] p-6 text-white">
+        Gradient Background Example
+      </div>
+
+      <p className="text-[var(--color-secondary-text)]">
+        This is secondary text
+      </p>
+      <p className="text-[var(--color-pink)]">This is pink text</p>
+      <p className="text-[var(--color-green)]">This is green text</p>
+
+      <button className="bg-[var(--color-purple)]  px-4 py-2 rounded-md text-white">
+        Click Me
+      </button>
+    </div>
+  );
 }
