454df78a1b8d5cad0ac92bf9a43cef017cd52c16
nihalnaveed12 <nihalnaveed50@gmail.com>
2025-03-07 14:53:59 +0500

    Add Hero Section

---
+++ src/components/Hero/hero.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Hero() {
+    return (
+        <div className="py-8 relative ">
+            <div className="absolute top-0">
+                <Image src="/left-grad.webp" alt="" width={1000} height={1000}/>
+            </div>
+            <div className="absolute top-0 right-0">
+                <Image src="/right-grad.webp" alt=""  width={1000} height={1000}/>
+            </div>
+
+          <div className="text-center">
+            <h1 className="font-sans text-4xl font-bold">Prop Firms Insights and Analytics</h1>
+            <h5 className="font-sans text-lg text-zinc-400">Across 500+ challenges and 40+ of the best firms.</h5>
+          </div>
+
+
+        </div>
+    )
+}
