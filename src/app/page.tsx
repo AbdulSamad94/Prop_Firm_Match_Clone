ab420af9bf831eabf9cbab8d5cfbd9dd652d2235
nihalnaveed12 <nihalnaveed50@gmail.com>
2025-03-07 14:58:13 +0500

    Add Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
+import Hero from "@/components/Hero/hero";
 import PropFirmTable from "@/components/PropFirmTable/PropFirmTable";
 import SubscriptionSection from "@/components/SubscriptionSection";
 import TestimonialCarousel from "@/components/TestimonialCarousel";
 export default function Home() {
   return (
     <div className="">
-      <div className="bg-[var(--color-bg)] text-[var(--color-text)] p-6">
-        This is a dark background with white text
-      </div>
-
-      <div className="bg-gradient-to-r from-[var(--gradient-from)] to-[var(--gradient-to)] p-6 text-white">
-        Gradient Background Example
-      </div>
+      <section>
+        <Hero />
+      </section>
 
       <section className="my-24 md:my-40">
         <PropFirmTable />
