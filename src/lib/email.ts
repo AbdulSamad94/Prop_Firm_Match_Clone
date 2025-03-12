b9181d44189b09aaa208f36ea0923f686bef6443
Muhammad Yousuf <codewithsaffy@gmail.com>
2025-03-13 00:32:43 +0500

    refactor email transport configuration to use Gmail service

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,16 +1,15 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_SERVER_HOST,
-    port: Number(process.env.EMAIL_SERVER_PORT),
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_SERVER_USER,
         pass: process.env.EMAIL_SERVER_PASSWORD,
     },
-    secure: process.env.EMAIL_SERVER_SECURE === 'true',
 });
 
 export async function sendVerificationEmail(email: string, otp: string) {
+    console.log(email, otp, process.env.EMAIL_FROM)
     try {
         await transporter.sendMail({
             from: process.env.EMAIL_FROM,
